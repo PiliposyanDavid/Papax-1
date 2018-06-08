c210809c21cfec26ea3758940830b7705ff34b00
haik19 <h.simonyan19@gmail.com>
2018-06-08 16:23:50 +0400

    reg page

--- app/src/main/java/com/papax/ag/papax/SignUpActivity.java
+++ app/src/main/java/com/papax/ag/papax/SignUpActivity.java
@@ -1,8 +1,10 @@
 package com.papax.ag.papax;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignUpActivity extends AppCompatActivity {
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.signup_acitivity_layout);
+		Button signUp = findViewById(R.id.sign_up_btn);
+		signUp.setOnClickListener(v -> {
+			Intent intent = new Intent(this, ModeChooserActivity.class);
+			startActivity(intent);
+		});
 	}
 }
