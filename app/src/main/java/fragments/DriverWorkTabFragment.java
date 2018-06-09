e9f999392efa0ccc2a61d2556769da8552023a6e
ZetBrush <zetbrush@gmail.com>
2018-06-09 06:44:34 +0400

    driver/passanger

--- app/src/main/java/fragments/DriverWorkTabFragment.java
+++ app/src/main/java/fragments/DriverWorkTabFragment.java
@@ -146,7 +146,7 @@ public class DriverWorkTabFragment extends Fragment implements OnMapReadyCallbac
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.work_tab_fragment_layout, container, false);
+		return inflater.inflate(R.layout.driver_work_tab_fragment_layout, container, false);
 	}
 
 
